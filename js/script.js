18bf3ce67ea65d5dc3f1e961cc487402c778e37b
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-05-11 22:42:44 +0400

    commit
    start project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let hend = prompt('choose Rock or Paper or Scissors');
+
+function RockPaperScissors(hend) {
+  let randomHend = Math.floor(Math.random() * 4);
+  console.log(randomHend);
+}
+
+RockPaperScissors(hend);
