e2c64621dcde5f1fb6850c1d1d884c3b58c268e1
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-05-12 15:35:58 +0400

    coomit
    xo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,54 @@
-let hend = prompt('choose Rock or Paper or Scissors');
+let btn = document.getElementById('startGame');
 
-function RockPaperScissors(hend) {
-  let randomHend = Math.floor(Math.random() * 4);
-  console.log(randomHend);
+let hend;
+
+function handdd() {
+  hend = prompt('choose Rock or Paper or Scissors');
+  hend.toLowerCase();
+  return hend;
 }
 
-RockPaperScissors(hend);
+btn.addEventListener('click', function () {
+  // hend = prompt('choose Rock or Paper or Scissors');
+  // hend.toLowerCase();
+  // return hend;
+  handdd();
+});
+
+console.log(handdd());
+
+let randomHend;
+
+function RockPaperScissors() {
+  randomHend = Math.floor(Math.random() * 3) + 1;
+  // console.log(randomHend);
+
+  if (randomHend === 1) {
+    randomHend = 'Rock';
+    // console.log(randomHend);
+  } else if (randomHend === 2) {
+    randomHend = 'Paper';
+  } else if (randomHend === 3) {
+    randomHend = 'Scissors';
+  }
+  return randomHend;
+}
+
+RockPaperScissors();
+
+// let pcHend = RockPaperScissors();
+
+// console.log(RockPaperScissors());
+
+function whowinner(hend, RockPaperScissors) {
+  console.log(RockPaperScissors);
+
+  if (
+    (RockPaperScissors === 'Rock' && hend === 'Paper') ||
+    hend === 'Scissors'
+  ) {
+    alert('lose');
+  }
+}
+
+whowinner(hend, RockPaperScissors());
